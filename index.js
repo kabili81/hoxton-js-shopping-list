21aa0e7247e75cc866f2db34e3c46396fd45db7b
Kabil Ibrahimi <kabili81@hotmail.com>
2022-06-16 09:26:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//Instructions
+//- Use these items and it's respective prices as a starting point: Milk at £1,20, Cocoa at £2,00, Salad at £2,00, Carrots at £2,00, Tomatoes at £2,50, Ready meals at £ 5,00
+//- Store this information using the most appropriated data structures we've studied so far
+//- Have a prompt to add an item to the list (you can have two prompts for the item and its price)
+//- Output a list displaying each item and its price
+//- Output the total at the end
+
+let products = ["Milk" + +  1.20, "Cocoa", "Salad", "Carrots", "Tomatoes", "Ready meals"]
+
+products.push("Banana")
+products.push("Orange")
+
+for (let product of products) {
+    console.log(`${product}`)
+}
+
+
+
+
+
+
+
